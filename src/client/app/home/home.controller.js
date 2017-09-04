66eaaa57c5e55df6645e5911b50888180b9c88c3
mr-code-maker <carlos@codecery.com>
2017-09-04 12:04:59 -0500

    Creates parallax effect

--- src/client/app/home/home.controller.js
+++ src/client/app/home/home.controller.js
@@ -13,6 +13,26 @@
     var home = this;
     var shell = $scope.shell;
 
+    var viewport = $('.image-space').height();
+    var body = $('body').height();
+
+    var floatSection = $('#float-section').offset().top;
+
+    $(window).scroll(function (event) {
+      var viewport = $('.image-space').height();
+      var body = $('body').height();
+      var difference = viewport-body;
+      if(difference > 0){
+        var scroll = $(window).scrollTop();
+        if(scroll >= difference){
+          var distance = scroll-difference;
+          $('#float-section').css({top:-(distance)});
+          $('#float-section').height($('#float-section').height()-distance);
+          console.log($('#float-section').height());
+        }
+      }
+    });
+
 
   };
 })();
