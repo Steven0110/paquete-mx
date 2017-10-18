1e25b96c25b6735fe41202ec3fa4a938159a2907
mr-code-maker <carlos@codecery.com>
2017-10-18 13:26:30 -0500

    Creates directive for card form

--- src/client/app/dashboard/paymentMethod/paymentMethod.controller.js
+++ src/client/app/dashboard/paymentMethod/paymentMethod.controller.js
@@ -15,7 +15,18 @@
 
     paymentMethod.labels = shell.labels.paymentMethod;
 
-    console.log(shell.labels.paymentMethod);
+    // paymentMethod.card = {};
+    // paymentMethod.card = {
+    //     name       : "Carlos Canizal Zuñiga",
+    //     number     : "4242424242424242",
+    //     cvc        : "123",
+    //     expMonth  : 1,
+    //     expYear   : 2018
+    // }
+
+    paymentMethod.send =function(response){
+      console.log(response);
+    }
     
   };
 })();
--- src/client/app/directives/card/card.directive.js
+++ src/client/app/directives/card/card.directive.js
@@ -9,7 +9,8 @@ function cardForm(userApi){
     restrict: 'EA',
     templateUrl: 'app/directives/card/card.form.html',
     scope: {
-      labels    : "="
+      labels    : "=",
+      sendForm  : "&"
     },
     bindToController: true,
     controllerAs: 'card',
@@ -17,15 +18,15 @@ function cardForm(userApi){
 
       var card = this;
 
-      card.labes = $scope.labels;
-
-      card.data = {
-        name       : "Carlos Canizal",
-        number     : "4242424242424242",
-        cvc        : "123",
-        expMonth  : 1,
-        expYear   : 2018
-      }
+      // if(!card.info){
+        card.info = {
+          name       : "Carlos Canizal",
+          number     : "4242424242424242",
+          cvc        : "123",
+          expMonth  : 1,
+          expYear   : 2018
+        }
+      // }
 
       card.months = [];
       for(var i=1; i<=12; i++){
@@ -35,6 +36,15 @@ function cardForm(userApi){
       card.years = [];
       for(var j=2017; j<=2030; j++){
         card.years.push(j);
+      }
+
+      card.send = function(){
+        if(card.form.$valid){
+          var data = card.info;
+          card.sendForm({response:{result:true, data:data}});
+        }else{
+          alert('invalid form');
+        }
       }
 
      
