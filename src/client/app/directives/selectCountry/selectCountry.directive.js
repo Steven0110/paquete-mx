46d283fef48ef67a7c5fb380f4276828f8546fc4
mr-code-maker <carlos@codecery.com>
2017-11-21 11:50:55 -0600

    Get cotizacion in controller

--- src/client/app/directives/selectCountry/selectCountry.directive.js
+++ src/client/app/directives/selectCountry/selectCountry.directive.js
@@ -2,9 +2,9 @@ angular
   .module('app.core')
   .directive('selectCountry',selectCountry);
 
-selectCountry.$inject = [];
+selectCountry.$inject = ['$document'];
 
-function selectCountry(){
+function selectCountry($document){
   return{
     require: 'ngModel',
     templateUrl: 'app/directives/selectCountry/selectCountry.template.html',
@@ -36,6 +36,20 @@ function selectCountry(){
         vm.country =  item;
         vm.show = false;
       }
+
+      $document.on('click', function(element){
+        if(vm.list.length > 0){
+          if(element.target && element.target.className){
+            var item = element.target.className;
+            // console.log(item);
+            if(!item.includes('country-name') && !item.includes('country-search') && !item.includes('fa-angle-down')){
+              vm.search ="";
+              vm.show = false;
+              $scope.$apply();
+            }
+          }
+        }
+      });
 
       
     }
--- src/client/app/home/home.controller.js
+++ src/client/app/home/home.controller.js
@@ -5,9 +5,9 @@
     .module('app.core')
     .controller('Home',Home);
 
-  Home.$inject = ['$state','$scope','$q','$timeout','template','rateApi'];
+  Home.$inject = ['$state','$scope','$q','$timeout','template','rateApi','Dialog'];
 
-  function Home($state, $scope, $q, $timeout, template, rateApi){
+  function Home($state, $scope, $q, $timeout, template, rateApi, Dialog){
     // jshint validthis: true 
     var home = this;
     var shell = $scope.shell;
@@ -18,25 +18,78 @@
     home.options = [];
     home.international = false;
 
-    home.shipping ={
-      from:{
-        search: null,
-        data: {},
-        country: null
-      },
-      to:{
-        search: null,
-        data: {},
-        country: null
-      },
-      type: "envelope",
-      package:{
-        weight: "1",
-        width: "25",
-        length: "25",
-        height: "25"
+    /*nuevo diseño*/
+    home.weightList = [1,2,3,4,5,6,7,8,9,10];
+    home.weightDocuments = 1;
+    home.selectType = function(type){
+      home.shipping.type = type;
+      if(type == 'box'){
+        home.documentOpen = false;
+        home.packageOpen = !home.packageOpen;        
+      }
+      else if(type == 'envelope'){
+        home.packageOpen = false;
+        home.documentOpen = !home.documentOpen;        
       }
     }
+    home.initialize ={
+      from:{
+        zip: null,
+        data: {},
+        country: "MX"
+      },
+      to:{
+        zip: null,
+        data: {},
+        country: "MX"
+      },
+      type: null,
+      valueDeclared: null,
+      insurance: false,
+      packages:[{
+        weight: null,
+        width: null,
+        length: null,
+        height: null
+      }
+      ]
+    };
+
+    home.shipping = JSON.parse(JSON.stringify(home.initialize));
+
+    home.addPackage = function(){
+      home.shipping.packages.push({
+        weight: null,
+        width: null,
+        length: null,
+        height: null
+      })
+    }
+
+    home.removePackage = function(index){
+      home.shipping.packages.splice(index,1)
+    }
+    /*nuevo diseño*/
+
+    // home.shipping ={
+    //   from:{
+    //     search: null,
+    //     data: {},
+    //     country: null
+    //   },
+    //   to:{
+    //     search: null,
+    //     data: {},
+    //     country: null
+    //   },
+    //   type: "envelope",
+    //   package:{
+    //     weight: "1",
+    //     width: "25",
+    //     length: "25",
+    //     height: "25"
+    //   }
+    // }
 
     home.setService = function(service){
 
@@ -158,122 +211,148 @@
       $('.benefits-'+index).slideUp(500);
     };
 
-
-
     home.send = function(){
+      if(home.shippingForm.fromZip.$valid){
+        if(home.shippingForm.toZip.$valid){
+          if(home.shipping.type == 'box' || home.shipping.type == 'envelope'){
+            if(!home.shipping.insurance || (home.shipping.insurance && home.shipping.valueDeclared && home.shipping.valueDeclared > 0)){
+              if(home.shippingForm.$valid){
 
-      if(home.shippingForm.$valid){
-        home.services = [];
-        var fromCountry = home.shipping.from.country.code;
-        var toCountry = home.shipping.to.country.code;
-        home.international =  shell.isInternational(fromCountry, toCountry);
-        console.log('international', home.international);
-        if(home.shipping.type == 'box' || home.shipping.type == 'documents'){
-          home.rated =  true;
-          home.searching =  true;
-          var viewport = $('.image-space').innerHeight();
-          var body = $('body').innerHeight();
-          var topContent = $('.image-space').innerHeight();
-          if(viewport <= body){
-            $('body,html').stop().animate({scrollTop:topContent/2},1000);
-          }else{
-            $('body,html').stop().animate({scrollTop:topContent/2},1000);
-          }
-          
-          var services = [{code:"ups", international:true},{code:"fedex",international:true},{code:"redpack",international:false}];
-
-          var fromZip;
-          if(home.shipping.from.data && home.shipping.from.data.zip){
-            fromZip =  home.shipping.from.data.zip;
-          }else{
-            fromZip =  home.shipping.from.search;
-          }
-
-          var toZip;
-          if(home.shipping.to.data && home.shipping.to.data.zip){
-            toZip =  home.shipping.to.data.zip;
-          }else{
-            toZip =  home.shipping.to.search;
-          }
-
-          var fromLatLng = {
-            lat: null,
-            lng: null
-          }
-
-          if(home.shipping.from.data && home.shipping.from.data.latitude && home.shipping.from.data.longitude){
-            fromLatLng.lat = home.shipping.from.data.latitude;
-            fromLatLng.lng = home.shipping.from.data.longitude;
-          }
-
-          var toLatLng = {
-            lat: null,
-            lng: null
-          }
-
-          if(home.shipping.to.data && home.shipping.to.data.latitude && home.shipping.to.data.longitude){
-            toLatLng.lat = home.shipping.to.data.latitude;
-            toLatLng.lng = home.shipping.to.data.longitude;
-          }          
-
-          var rate = {
-            "from": {
-              "zip": fromZip,
-              "country": fromCountry,
-              "geolocation":fromLatLng
-            },
-            "to": {
-              "zip": toZip,
-              "country": toCountry,
-              "geolocation": toLatLng
-            },
-            "packages":[home.shipping.package]
-          };
-          var promises = [];
-          angular.forEach(services,function(service){
-
-            var runRate = true; 
-            if(home.international && !service.international){
-              console.log('service no international'+service.code);
-              runRate = false;
+                console.log(home.shipping);
+                
+              }else{
+                Dialog.showError('Verifica las dimensiones de los paquetes, recuerda que deben ser numéricos mayores a 0','Dimensiones de los paquetes');  
+              }
+            }else{
+              Dialog.showError('El valor declarado debe ser mayor a 0 si deseas asegurar.','¿Cuál es el valor declarado?');
             }
-
-            if(runRate){
-              var params = {
-                "type":service.code,
-                "rate": rate
-              };
-
-              promises.push(
-                rateApi.rate(service,params).then(function(response){
-                  // console.log(response);
-                  if(response.services){
-                    home.services = home.services.concat(response.services);
-                  }
-                  var deferred = $q.defer();
-                  deferred.resolve(response);
-                  return deferred.promise;
-                },function(err){
-                  console.log(err);
-                  var deferred = $q.defer();
-                  deferred.reject(err);
-                  return deferred.promise;
-                })
-              );
-            }
-          });
-          $q.all(promises).then(function(result){
-          },function(err){
-            console.log(err);
-          }).finally(function(){
-            console.log(home.services);
-            home.searching = false;
-          });
-
-          
+          }else{
+            // alert('Selecciona si tu envio son documentos o paquetes.');
+            Dialog.showError('Selecciona si tu envío son documentos o paquetes.','¿Qué envías?');
+          }
+        }else{
+         Dialog.showError('Código postal de destino es requerido.','¿Hacia dónde envías?'); 
         }
+      }else{
+       Dialog.showError('Código postal de origen es requerido.','¿De dónde envías?');
       }
     };
+
+
+    // home.send = function(){
+
+    //   if(home.shippingForm.$valid){
+    //     home.services = [];
+    //     var fromCountry = home.shipping.from.country.code;
+    //     var toCountry = home.shipping.to.country.code;
+    //     home.international =  shell.isInternational(fromCountry, toCountry);
+    //     console.log('international', home.international);
+    //     if(home.shipping.type == 'box' || home.shipping.type == 'documents'){
+    //       home.rated =  true;
+    //       home.searching =  true;
+    //       var viewport = $('.image-space').innerHeight();
+    //       var body = $('body').innerHeight();
+    //       var topContent = $('.image-space').innerHeight();
+    //       if(viewport <= body){
+    //         $('body,html').stop().animate({scrollTop:topContent/2},1000);
+    //       }else{
+    //         $('body,html').stop().animate({scrollTop:topContent/2},1000);
+    //       }
+          
+    //       var services = [{code:"ups", international:true},{code:"fedex",international:true},{code:"redpack",international:false}];
+
+    //       var fromZip;
+    //       if(home.shipping.from.data && home.shipping.from.data.zip){
+    //         fromZip =  home.shipping.from.data.zip;
+    //       }else{
+    //         fromZip =  home.shipping.from.search;
+    //       }
+
+    //       var toZip;
+    //       if(home.shipping.to.data && home.shipping.to.data.zip){
+    //         toZip =  home.shipping.to.data.zip;
+    //       }else{
+    //         toZip =  home.shipping.to.search;
+    //       }
+
+    //       var fromLatLng = {
+    //         lat: null,
+    //         lng: null
+    //       }
+
+    //       if(home.shipping.from.data && home.shipping.from.data.latitude && home.shipping.from.data.longitude){
+    //         fromLatLng.lat = home.shipping.from.data.latitude;
+    //         fromLatLng.lng = home.shipping.from.data.longitude;
+    //       }
+
+    //       var toLatLng = {
+    //         lat: null,
+    //         lng: null
+    //       }
+
+    //       if(home.shipping.to.data && home.shipping.to.data.latitude && home.shipping.to.data.longitude){
+    //         toLatLng.lat = home.shipping.to.data.latitude;
+    //         toLatLng.lng = home.shipping.to.data.longitude;
+    //       }          
+
+    //       var rate = {
+    //         "from": {
+    //           "zip": fromZip,
+    //           "country": fromCountry,
+    //           "geolocation":fromLatLng
+    //         },
+    //         "to": {
+    //           "zip": toZip,
+    //           "country": toCountry,
+    //           "geolocation": toLatLng
+    //         },
+    //         "packages":[home.shipping.package]
+    //       };
+    //       var promises = [];
+    //       angular.forEach(services,function(service){
+
+    //         var runRate = true; 
+    //         if(home.international && !service.international){
+    //           console.log('service no international'+service.code);
+    //           runRate = false;
+    //         }
+
+    //         if(runRate){
+    //           var params = {
+    //             "type":service.code,
+    //             "rate": rate
+    //           };
+
+    //           promises.push(
+    //             rateApi.rate(service,params).then(function(response){
+    //               // console.log(response);
+    //               if(response.services){
+    //                 home.services = home.services.concat(response.services);
+    //               }
+    //               var deferred = $q.defer();
+    //               deferred.resolve(response);
+    //               return deferred.promise;
+    //             },function(err){
+    //               console.log(err);
+    //               var deferred = $q.defer();
+    //               deferred.reject(err);
+    //               return deferred.promise;
+    //             })
+    //           );
+    //         }
+    //       });
+    //       $q.all(promises).then(function(result){
+    //       },function(err){
+    //         console.log(err);
+    //       }).finally(function(){
+    //         console.log(home.services);
+    //         home.searching = false;
+    //       });
+
+          
+    //     }
+    //   }
+    // };
 
 
     var floatSection = $('#float-section').offset().top;
